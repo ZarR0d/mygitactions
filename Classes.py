e53f9b0d740550603630a5442337eebb1ed60aec
g7rodrc <cesar.rodriguez@swisslog.com>
2023-10-02 15:55:37 -0600

    classes.py corrected after linter check - V5

--- Classes.py
+++ Classes.py
@@ -6,9 +6,13 @@
 
 
 class Point:  # By convention classes start with a capital letter (Pascal naming convention)
-    def __init__(self, x, y):  # Self is a reference to the current ojbject #This method is a constructor. This methjod is used to contruct or create an object
+    def __init__(
+        self, x, y
+    ):  # Self is a reference to the current ojbject #This method is a constructor. 
+        # This methjod is used to contruct or create an object
         self.x = x
         self.y = y
+
     def move(self):
         print("move")
 
@@ -16,11 +20,11 @@ class Point:  # By convention classes start with a capital letter (Pascal naming
         print("draw")
 
 
-point1 = Point(0,0)  # Instanciation of the class
+point1 = Point(0, 0)  # Instanciation of the class
 point1.x = 10
 point1.y = 20
 print(point1.x)
 point1.draw()  # Don't forget the parenthesis. Objects are called as functions
-point2 = Point(0,0)
+point2 = Point(0, 0)
 point2.x = 1
 print(point2.x)

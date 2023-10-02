9ee33856bf0fbe333318b9e78b87c8c0aff0534d
g7rodrc <cesar.rodriguez@swisslog.com>
2023-10-02 15:08:24 -0600

    Example of classes added

---
+++ Classes.py
@@ -0,0 +1,23 @@
+#We use clases to define NEW TYPES. These types can have methods that we define in the body of the class and they also
+#have attributes that we can set anywhere in our programs
+
+#Basic types examples in Python:Numbers, Strings and Booleans
+#Complex types examples in Python: Lists, Dictionaries
+
+class Point: #By convention classes start with a capital letter (Pascal naming convention)
+    def move(self):
+        print("move")
+    def draw(self):
+        print("draw")
+
+
+
+point1 = Point()    #Instanciation of the class
+point1.x = 10
+point1.y = 20
+print(point1.x)
+point1.draw()       #Don't forget the parenthesis. Objects are called as functions
+
+point2 = Point()
+point2.x = 1
+print(point2.x)
